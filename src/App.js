b2681018daaeb62a95a86332bc325de30cbcaa9f
Paul <paul@teamsense.com>
2022-05-12 12:51:00 -0700

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Form } from "react-bootstrap";
 
-function doesWin(firstCardVal, myCardVal, secondCardVal)
-{
-  const wins = myCardVal > firstCardVal && myCardVal < secondCardVal
-  console.log(wins);
-}
-
+import "./scss/main.scss";
 
 function App() {
+  function doesWin(firstCardVal, myCardVal, secondCardVal) {
+    const wins = myCardVal > firstCardVal && myCardVal < secondCardVal;
+    console.log(wins);
+  }
+
   doesWin(2, 5, 7);
   doesWin(2, 8, 7);
   doesWin(2, 2, 7);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <h1>Your Cash: $100</h1>
+
+      <section>(Your Cards)</section>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Amount to Bet?</Form.Label>
+        <Form.Control type="number" placeholder="Enter Amount" />
+      </Form.Group>
+      <Button variant="primary" size="lg">
+        BET!
+      </Button>
+
+      <Button variant="danger" size="lg">
+        PASS!
+      </Button>
+    </Container>
   );
 }
 
