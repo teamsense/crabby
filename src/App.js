38f83460d1ab31cf592457d0e97e3db8e831630a
Mike Capozzi <michael.capozzi@teamsense.com>
2022-05-12 12:26:39 -0700

    Wrote function to return whether you win a round based on card val coming in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function doesWin(firstCardVal, myCardVal, secondCardVal)
+{
+  const wins = myCardVal > firstCardVal && myCardVal < secondCardVal
+  console.log(wins);
+}
+
+
 function App() {
+  doesWin(2, 5, 7);
+  doesWin(2, 8, 7);
+  doesWin(2, 2, 7);
   return (
     <div className="App">
       <header className="App-header">
